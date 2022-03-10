dae0d5fb5fb35bf1d7b8e7a1136d2cfbd57d67ba
taalojarvi <sreedevan05@gmail.com>
2022-03-10 19:25:41 +0530

    drivers: qcom: Fix a mismerge
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

--- drivers/soc/qcom/smp2p.c
+++ drivers/soc/qcom/smp2p.c
@@ -2,7 +2,6 @@
 /*
  * Copyright (c) 2015, Sony Mobile Communications AB.
  * Copyright (c) 2012-2013, 2018-2020 The Linux Foundation. All rights reserved.
-<<<<<<< HEAD
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -12,8 +11,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-=======
->>>>>>> 460cb8ef9d (soc: qcom: Checkout smem and smp2p from v5.4)
  */
 
 #include <linux/interrupt.h>
