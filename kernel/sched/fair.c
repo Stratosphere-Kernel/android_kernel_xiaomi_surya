bca7e1dc453358917751720291694d4d539c5615
Rick Yiu <rickyiu@google.com>
2022-06-01 19:11:47 +0800

    sched/fair: schedule lower priority tasks from little cores
    With scheduler placement hint, there still could be several boosted
    tasks contending for big cores. On chipset with fewer big cores, it
    might cause problems like jank. To improve it, schedule tasks of prio
    >= DEFAULT_PRIO from little cores if they could fit, even for tasks
    that prefer high capacity cpus, since such prio means they are less
    important.
    
    Bug: 158936596
    Test: tasks scheduled as expected
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: Ic0cc06461818944e3e97ec0493c0d9c9f1a5e217
    Signed-off-by: Aruato <aruato@qq.com>
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7569,7 +7569,7 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 	if (is_min_capacity_cpu(cpu)) {
 		if (task_boost_policy(p) == SCHED_BOOST_ON_BIG ||
 			task_boost > 0 ||
-			schedtune_task_boost(p) > 0 ||
+			schedtune_task_boost(p) > 0 && p->prio <= DEFAULT_PRIO ||
 			walt_should_kick_upmigrate(p, cpu))
 			return false;
 	} else { /* mid cap cpu */
@@ -8067,7 +8067,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		 */
 		if ((prefer_idle && best_idle_cpu != -1) ||
 		    (boosted && (best_idle_cpu != -1 || target_cpu != -1))) {
-			if (boosted) {
+			if (boosted && p->prio <= DEFAULT_PRIO) {
 				if (!next_group_higher_cap)
 					break;
 			} else {
@@ -8347,7 +8347,7 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 	int placement_boost = task_boost_policy(p);
 	u64 start_t = 0;
 	int next_cpu = -1, backup_cpu = -1;
-	int boosted = (schedtune_task_boost(p) > 0 || per_task_boost(p) > 0);
+	int boosted = (schedtune_task_boost(p) > 0 && p->prio <= DEFAULT_PRIO || per_task_boost(p) > 0);
 	int start_cpu;
 	bool about_to_idle = (cpu_rq(cpu)->nr_running < 2);
 
