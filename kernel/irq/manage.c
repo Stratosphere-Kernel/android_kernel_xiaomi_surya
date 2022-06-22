F1xy-kernels/RAPHAEL_Kernel

kernel: irq: manage: use a different way of affining perf IRQs
Adjust cpumask directly.

Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

kernel/irq/manage.c
@@ -385,6 +385,9 @@ int irq_setup_affinity(struct irq_desc *desc)
 	if (cpumask_empty(&mask))
 		cpumask_copy(&mask, cpu_online_mask);
 
+	if (irqd_has_set(&desc->irq_data, IRQF_PERF_CRITICAL))
+		cpumask_copy(&mask, cpu_perf_mask);
+
 	if (node != NUMA_NO_NODE) {
 		const struct cpumask *nodemask = cpumask_of_node(node);
 
@@ -1318,9 +1321,6 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 			if (ret)
 				goto out_thread;
 		}
-
-		if (new->flags & IRQF_PERF_CRITICAL)
-			affine_one_perf_thread(new->thread);
 	}
 
 	/*

Stratosphere-Kernel/android_kernel_xiaomi_surya

Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

kernel/irq/manage.c
@@ -426,6 +426,9 @@ int irq_setup_affinity(struct irq_desc *desc)
 	if (cpumask_empty(&mask))
 		cpumask_copy(&mask, cpu_online_mask);
 
+	if (irqd_has_set(&desc->irq_data, IRQD_PERF_CRITICAL))
+		cpumask_copy(&mask, cpu_perf_mask);
+
 	if (node != NUMA_NO_NODE) {
 		const struct cpumask *nodemask = cpumask_of_node(node);
 